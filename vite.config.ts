1d9df6f99500ffc6539191e0af4cd815857e270e
sarb7479 <sarbzcode@gmail.com>
2025-10-18 11:47:11 -0300

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
   server: {
     proxy: {
@@ -12,4 +13,5 @@ export default defineConfig({
       },
     },
   },
+  
 })
